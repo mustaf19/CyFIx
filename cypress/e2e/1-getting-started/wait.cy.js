2aac588ea03653bf6c11b1a9f215e05518e5a30f
mustaf19 <mustafeez0001@gmail.com.com>
2023-04-21 23:03:22 +0530

    added test case for implicit and explicit waits-I

---
+++ cypress/e2e/1-getting-started/wait.cy.js
@@ -0,0 +1,21 @@
+describe("implicit-explicit wait", () => {
+
+    it('Implicit Wait', () => {
+        cy.visit("https://www.saucedemo.com/")
+        cy.get("input[placeholder='Username']").type("standard_user")
+        cy.get("input[placeholder='Password']").type('secret_sauce')
+        cy.get("input[type='submit']").click().wait(5000)
+        // cy.contains("Sports, Healthcare & more")
+    })
+
+    it('explicit Wait', () => {
+        cy.visit("https://www.saucedemo.com/")
+        cy.get("input[placeholder='Username']").type("standard_user")
+        cy.get("input[placeholder='Password']").type('secret_sauce')
+        cy.get("input[type='submit']").click()
+        cy.get("div[id='page_wrapper']").should('be.visible')
+        // cy.contains("Sports, Healthcare & more")
+    })
+
+
+})
